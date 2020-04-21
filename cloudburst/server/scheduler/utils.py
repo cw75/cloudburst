270b8f86639fc3339c5c42cdad463180378dc8a9
cw75 <cgwu0530@gmail.com>
2020-04-21 01:04:40 +0000

    add import zmq

--- cloudburst/server/scheduler/utils.py
+++ cloudburst/server/scheduler/utils.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+import zmq
+
 from anna.lattices import SetLattice
 
 import cloudburst.server.utils as sutils
