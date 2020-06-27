d4ab9e98525252dc755e9e8c4b0f807dab5496b1
cw75 <cgwu0530@gmail.com>
2020-06-27 23:52:34 +0000

    160 replica

--- cloudburst/server/scheduler/create.py
+++ cloudburst/server/scheduler/create.py
@@ -58,7 +58,7 @@ def create_function(func_create_socket, kvs, consistency=MULTI):
 
 
 def create_dag(dag_create_socket, pusher_cache, kvs, dags, policy,
-               call_frequency, num_replicas=3):
+               call_frequency, num_replicas=160):
     serialized = dag_create_socket.recv()
 
     dag = Dag()
