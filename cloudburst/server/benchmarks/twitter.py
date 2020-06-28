76f3a76a9df4eb17644b6704c1342d29e4d24a5c
cw75 <cgwu0530@gmail.com>
2020-06-28 05:07:50 +0000

    read only

--- cloudburst/server/benchmarks/twitter.py
+++ cloudburst/server/benchmarks/twitter.py
@@ -158,7 +158,7 @@ def run(cloudburst_client, num_requests, create, sckt):
             refs.append(CloudburstReference(tid, True))
             arg_map = {'tweet': refs}
             start = time.time()
-            if random.random() < 0.1:
+            if random.random() < 0:
                 #logging.info('post')
                 cb_future = cloudburst_client.call_dag(dag_name, arg_map, False, MULTI, None, uid)
                 result_id = cb_future.obj_id
